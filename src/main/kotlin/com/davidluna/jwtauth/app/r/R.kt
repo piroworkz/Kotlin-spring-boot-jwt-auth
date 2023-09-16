6da996c68ebc7f2c759a9aaaa47298df0acaf998
David Eduardo Luna Cervantes <lunade@globalhitss.com>
2023-09-16 14:32:19 -0600

    CREATED RESOURCES OBJECT

---
+++ src/main/kotlin/com/davidluna/jwtauth/app/r/R.kt
@@ -0,0 +1,39 @@
+package com.davidluna.jwtauth.app.r
+
+sealed interface R {
+
+    object Strings {
+        const val EMPTY_STRING = ""
+        const val SCOPE_IO = "scopeIO"
+        const val SCOPE_MAIN = "scopeMain"
+        const val CSRF: String = "_csrf"
+        const val AUTHORIZATION: String = "Authorization"
+        const val ALLOWED_METHODS: String = "POST"
+        const val ALLOW_ALL: String = "*"
+        const val AUTH_PREFIX = "/auth/**"
+        const val GREET_PREFIX = "/greet/**"
+    }
+
+    object ProtectedPaths {
+        const val GREET_PREFIX = "/greet"
+        const val HELLO = "/hello"
+    }
+
+    object Paths {
+        const val AUTH_PREFIX = "/auth"
+        const val LOGIN = "/login"
+        const val SIGNUP = "/signup"
+        const val LOGOUT = "/logout"
+        const val SALUTE = "/salute"
+        const val GET_USER = "/getUser"
+    }
+
+    object JWTConfig {
+        val expiration = System.currentTimeMillis().plus(600L * 1000L)
+        const val CLAIM_USERNAME = "USERNAME"
+        const val CLAIM_AUTHORITIES = "AUTHORITIES"
+        const val ISSUER = "REST_API"
+        const val JWT_SECRET = "JWT_SECRET"
+        const val SUBJECT = "Json_Web_Token"
+    }
+}
